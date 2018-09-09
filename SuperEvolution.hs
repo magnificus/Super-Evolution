2815e907b804ea6baa26754c214ce6488ccde510
magnificus <tobiaselinder@gmail.com>
2018-09-09 19:50:41 +0200

    added more randomize integration

--- SuperEvolution.hs
+++ SuperEvolution.hs
@@ -3,6 +3,9 @@ import Data.Map
 import System.Random
 type Env = Map Char Double
 
+mutateChildChance = 0.1
+mutateLeafChance = 0.1
+
 defaultMap = fromList [('x', 1.0), ('y', 2.0)]
 defaultTree = (Node Sub (Node Add (Leaf (Var 'x')) (Leaf (Lit 2))) (Leaf (Var 'y')))
 
@@ -47,6 +50,7 @@ defaultTU2 = TranslationUnit ('x', 'w') (Var 'x') Sub
 defaultTU3 = TranslationUnit ('w', 'f') (Lit 3) Mul 
 
 treeMap = fromList [('f', defaultTU1), ('x', defaultTU2), ('w', defaultTU3)]
+availableFunctions = [Add,Sub,Mul]
 
 charToTree = (!) treeMap
 
@@ -55,13 +59,19 @@ toTree t n
    | n > 1 = Node (function t)  (toTree (charToTree . fst $ childrenNodes t) (n-1)) (toTree  (charToTree . snd $ childrenNodes t) (n-1))
    | otherwise =  Leaf (singleNode t)
 
-getTUChar = (!!) (keys treeMap)
+getTUChar :: Double -> Char
+getTUChar f = (!!) (keys treeMap) (floor $ f* (fromIntegral $ size treeMap))
+
+getLeaf :: Double -> Double -> Leaf
+getLeaf f1 f2 = if (f1 < 0.5) then (Lit f2) else (Var (getTUChar f2))
+
+getMutatedChildren (c1,c2) f1 f2 f3 f4 = (maybeChange c1 f1 f2, maybeChange c2 f3 f4)
+   where maybeChange c f1 f2 = if (f1 < mutateChildChance) then getTUChar f2 else c
+
+getMutatedLeaf l f1 f2 = if (f1 < mutateLeafChance) then getLeaf f2 else l
 
 mutateNode n = do
-  g <- newStdGen
-  let (x,g) = randomR (1.0::Float,6.0::Float) g
-  return g
-  --randomR (1,6) g
- -- return 1
-  --rs <- randomList (n-1)
-  --return (r:rs) 
+  let newChildren = getMutatedChildren (childrenNodes n) <$> randDouble <*> randDouble <*> randDouble <*> randDouble
+  let newLeaf = getMutatedLeaf 
+  newChildren
+  where randDouble = randomRIO (0.0::Double,0.9999::Double)
